22d3fefc465f391cfd1c95b2afb9d0d3e74fb08b
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2020-10-06 08:23:07 -0500

    Update win.py
    awa

--- win.py
+++ win.py
@@ -7,6 +7,6 @@ while True:
         a = input("请重新输入产品密钥:")
     else:
         os.system("slmgr /ipk " + a)
-        os.system("slmgr /skms kms.03k.org")
+        os.system("slmgr /skms kms.03k.org") #此命令需要管理员权限才可执行,导致全程序需要以管理员的身份运行
         os.system("slmgr /ato")
         break
