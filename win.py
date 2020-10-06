2e76deea4dfe0da207e869de587dd6a00ac87f0c
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2020-10-06 08:14:25 -0500

    Use kms.03k.org to active Windows

--- win.py
+++ win.py
@@ -1,2 +1,12 @@
 inport os
 
+
+a = input("请输入产品密钥:")
+while True:
+    if os.system("slmgr /ipk " + a) !=0:
+        a = input("请重新输入产品密钥:")
+    else:
+        os.system("slmgr /ipk " + a)
+        os.system("slmgr /skms kms.03k.org")
+        os.system("slmgr /ato")
+        break
