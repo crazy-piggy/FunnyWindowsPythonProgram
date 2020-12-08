9b81b84ce9e42ff3db3c10858badc2ec874088e8
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2020-12-08 21:41:37 +0800

    Update win.py

--- win.py
+++ win.py
@@ -2,11 +2,14 @@ import os
 
 
 a = input("请输入产品密钥:")
+b = input("请输入kms服务器地址:")
 while True:
     if os.system("slmgr /ipk " + a) !=0:
         a = input("请重新输入产品密钥:")
+    if os.system("slmgr /skms " + b) != 0:
+        b = input("请重新输入kms服务器地址或以管理员身份运行该程序:")
     else:
         os.system("slmgr /ipk " + a)
-        os.system("slmgr /skms kms.03k.org") #此命令需要管理员权限才可执行,导致全程序需要以管理员的身份运行
+        os.system("slmgr /skms " + b)
         os.system("slmgr /ato")
         break
